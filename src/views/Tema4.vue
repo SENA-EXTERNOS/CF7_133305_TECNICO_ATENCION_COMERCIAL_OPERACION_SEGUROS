994b1d627daefac31cf81221607f5144d71ee602
Andrea Botello <andrea.botellor@misena.edu.co>
2023-03-28 17:54:22 -0500

    Videos y Material

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -66,7 +66,7 @@
 
       figure.mb-5(data-aos="zoom-in")
         .video
-          iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+          iframe(width="560" height="315" src="https://www.youtube.com/embed/96pohadjEWE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
       .h4 Integración de datos
 
